c2fe56bf90dceff745a55f7d78b0fab0b04d2eb5
Mathieu Desvé <mathieu.desve@me.com>
2015-05-16 10:59:53 +0200

    First commit

---
+++ handler.js
@@ -0,0 +1,60 @@
+console.log('Loading function');
+var aws = require('aws-sdk');
+var transcoder = new aws.ElasticTranscoder({apiVersion: '2012-09-25'});
+
+exports.handler = function(event, context) {
+  console.log('Received event:', JSON.stringify(event, null, 2));
+  // Get the object from the event and show its content type
+  var bucket = event.Records[0].s3.bucket.name;
+  var key = event.Records[0].s3.object.key;
+  
+  if (bucket == 'chatanoo-medias-input') {
+    var mediaId = key.replace(/\.[^/.]+$/, "");
+    var outputs = [];
+    if (true) {
+      outputs = [
+        {
+          Key: mediaId + '/video.mp4',
+          PresetId: '1431764878757-w37ugf' /* Chatanoo - MP4 */
+        },
+        {
+          Key: mediaId + '/video.webm',
+          PresetId: '1431765105700-dodtxt' /* Chatanoo - WebM */
+        },
+        {
+          Key: mediaId + '/video.flv',
+          PresetId: '1431765233842-9ulfbc' /* Chatanoo - WebM */
+        }
+      ];
+    } else {
+      outputs = [
+        {
+          Key: mediaId + '/audio.mp3',
+          PresetId: '1431766141904-lpijfs' /* Chatanoo - MP3 */
+        },
+        {
+          Key: mediaId + '/audio.ogg',
+          PresetId: '1431766187146-dooe52' /* Chatanoo - MP3 */
+        }
+      ];
+    }
+
+    var params = {
+      Input: {
+        Key: key
+      },
+      PipelineId: '1431762470072-gnq6il', /* chatanoo */
+      Outputs: outputs
+    };
+
+    transcoder.createJob(params, function(err, data) {
+      if (err) { 
+        console.log(err, err.stack);
+        context.fail('Error', "Error Creating Job: " + err);
+      } else {
+        console.log(data);
+        context.succeed();
+      }
+    });
+  }
+};
